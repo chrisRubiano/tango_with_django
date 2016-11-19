f2fe932eec4daeba3f6056d73e1ef39b7da2c7bf
Cristian Samaniego <chris.rubiano42@gmail.com>
2016-11-18 19:45:18 -0700

    experimenting with templates

--- rango/views.py
+++ rango/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 import os
+from rango.models import Category
+from rango.models import Page
 # Create your views here.
 
 
 def index(request):
-    context_dict = {'boldmessage': "crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    pages_list = Page.objects.order_by('views')
+    # for category in category_list:
+    #     for page in pages_list:
+    #         if category.name == page.category.name:
+    #             print(page.category.name)
+    context_dict = {'categories': category_list, 'pages': pages_list}
     return render(request, 'rango/index.html', context=context_dict)
-
-
-def amerika(request):
-    return HttpResponse("ke dijistes del amerika wey")
